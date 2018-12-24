0b5b3c54a952ece31c8469208bd4623333885dda
Pattarapon Lertratananont <pattaraponler@gmail.com>
2018-12-24 16:37:51 +0700

    http get post method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Demon';
+  constructor(private httpClient:HttpClient){}
+  name:string=''
+  age:number
+  found:boolean // ไว้สำหรับให้ app-html ตรวจสอบว่าจะแสดงอะไร
+  onNameKeyUp(event:any){
+    this.name=event.target.value
+    this.found = false
+  }
+
+  getProfile(){
+    this.httpClient.get(`https://my-json-server.typicode.com/techsithgit/json-faker-directory/profiles/?name=${this.name}`)
+    .subscribe(
+      (data:any[]) =>{
+        if(data.length){
+          this.age = data[0].age
+          this.found = true
+        }
+      } 
+    )
+  }
+
+  postProfile(){
+    this.httpClient.post(`https://my-json-server.typicode.com/techsithgit/json-faker-directory/profiles`,
+    {
+      name:'Ball',
+      age:20
+    })
+    .subscribe(
+      (data:any) =>{
+        console.log(data)
+      } 
+    )
+  }
+
 }
